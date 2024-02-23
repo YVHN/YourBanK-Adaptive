7e50663dd210bc4faacee2ce3f872521719f4524
Yevhen Suprunenko <gollandolion@gmail.com>
2024-02-23 19:53:26 +0100

    fixes

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,14 @@
+const body = document.querySelector('.body');
 const menu = document.querySelector('.menu-burger');
 const menuButton = document.querySelector('.menu-burger__button');
 
 function toggleState() {
   if (menu.classList.contains('active-menu')) {
     menu.classList.remove('active-menu');
+    body.classList.remove('block-scroll');
   } else {
     menu.classList.add('active-menu');
+    body.classList.add('block-scroll');
   }
 }
 
